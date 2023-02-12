ec1b104df78a915095bba52505a3620997426294
Dewi Mawaddah <dewimawaddahwarrohmah@gmail.com>
2023-02-12 10:46:51 +0700

    done negative updateProduct

--- test/updateProduct.js
+++ test/updateProduct.js
@@ -9,20 +9,45 @@ describe("Kasir Aja API Product", () => {
     });
     const token = response.body.data.accessToken;
 
-    //
+    //start positive
     const updateProductResponse = await request
-      .put("/products/5cadf7da-15fa-4848-98f6-3d86b0ebe643")
+      .put("/products/53c9fecb-c1f7-4bad-b8a5-9de5bfae4485")
       .send({
-        category_id: "811f547e-a24e-4f94-bfe1-b7ed7d11c026",
+        category_id: "811f547e-a24e-4f94-bfe1-b7ed7d11c030",
         code: "A314ASDDFIER3452",
-        name: "Roti",
-        price: "12000",
-        cost: "10000",
-        stock: "5",
+        name: "Yakult",
+        price: "13000",
+        cost: "12000",
+        stock: "20",
       })
       .auth(token, { type: "bearer" });
 
-    expect(updateProductResponse.body.data.name).to.eql("Roti");
-    //
+    expect(updateProductResponse.body.data.name).to.eql("Yakult");
+    //end positive
   });
+
+  //start negative
+  it("Failed update product dengan code kosong", async function () {
+    const response = await request.post("/authentications").send({
+      email: "dewi@gmail.com",
+      password: "123yayaya",
+    });
+    const token = response.body.data.accessToken;
+
+    const updateProductResponse = await request
+      .put("/products/53c9fecb-c1f7-4bad-b8a5-9de5bfae4485")
+      .send({
+        category_id: "811f547e-a24e-4f94-bfe1-b7ed7d11c045",
+        code: "",
+        name: "Yakult",
+        price: "13000",
+        cost: "12000",
+        stock: "20",
+      })
+      .auth(token, { type: "bearer" });
+
+    expect(updateProductResponse.body.status).to.eql("fail");
+    expect(updateProductResponse.body.message).to.eql('"code" is not allowed to be empty');
+  });
+  //end negative
 });
