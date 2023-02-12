897827398b31469ccc2700d161f9309619a73874
Dewi Mawaddah <dewimawaddahwarrohmah@gmail.com>
2023-02-12 08:00:28 +0700

    getProductList progres

---
+++ test/addProduct.js
@@ -0,0 +1,15 @@
+const request = require("supertest")("https://kasir-api.belajarqa.com");
+const expect = require("chai").expect;
+
+describe("Kasir Aja API Product", () => {
+  it("Success get product list", async function () {
+    const response = await request.post("/authentications").send({
+      email: "dewi@gmail.com",
+      password: "123yayaya",
+    });
+    const token = response.body.data.accessToken;
+
+    //
+    //
+  });
+});
--- test/getProductList.js
+++ test/getProductList.js
@@ -1,14 +1,21 @@
-const request = require('supertest')
-const expect = require('chai').expect
+const request = require("supertest")("https://kasir-api.belajarqa.com");
+const expect = require("chai").expect;
 
-before(function(){
-    
-})
+describe("Kasir Aja API Product", () => {
+  it("Success get product list", async function () {
+    const response = await request.post("/authentications").send({
+      email: "dewi@gmail.com",
+      password: "123yayaya",
+    });
+    const token = response.body.data.accessToken;
 
-describe('Kasir Aja API Product', () => {
-    const getResponse = request ('https://kasir-api.belajarqa.com')
-    .get('/products?page=1&q=taro&withStock=true&withCategory=true&categoryId=a8851b17-9de-4c66-bc16-d4279a9a7c77')
-    it('success get product list', async() => {
-        expect((await getResponse).status).to.eql(200)
-    })
-})
+    const getProductListResponse = await request.get("/products").auth(token, { type: "bearer" });
+
+    console.log(getProductListResponse.body);
+  });
+
+  //   const getResponse = request("https://kasir-api.belajarqa.com").get("/products?page=1&q=taro&withStock=true&withCategory=true&categoryId=a8851b17-9de-4c66-bc16-d4279a9a7c77");
+  //   it("success get product list", async () => {
+  //     expect((await getResponse).status).to.eql(200);
+  //   });
+});
