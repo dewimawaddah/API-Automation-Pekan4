2feeee621e0fb6bc2dc73a8fc9a2d48a194d6db0
Dewi Mawaddah <dewimawaddahwarrohmah@gmail.com>
2023-02-12 08:48:23 +0700

    done getProductList and addProduct

--- test/addProduct.js
+++ test/addProduct.js
@@ -2,7 +2,7 @@ const request = require("supertest")("https://kasir-api.belajarqa.com");
 const expect = require("chai").expect;
 
 describe("Kasir Aja API Product", () => {
-  it("Success get product list", async function () {
+  it("Success add product", async function () {
     const response = await request.post("/authentications").send({
       email: "dewi@gmail.com",
       password: "123yayaya",
@@ -10,6 +10,19 @@ describe("Kasir Aja API Product", () => {
     const token = response.body.data.accessToken;
 
     //
+    const addProductResponse = await request
+      .post("/products")
+      .send({
+        category_id: "811f547e-a24e-4f94-bfe1-b7ed7d11c026",
+        code: "A314ASDDFIER3452",
+        name: "Coklat",
+        price: "12000",
+        cost: "10000",
+        stock: "5",
+      })
+      .auth(token, { type: "bearer" });
+
+    expect(addProductResponse.body.message).to.eql("Product berhasil ditambahkan");
     //
   });
 });
--- test/updateProduct.js
+++ test/updateProduct.js
@@ -2,13 +2,14 @@ const request = require("supertest")("https://kasir-api.belajarqa.com");
 const expect = require("chai").expect;
 
 describe("Kasir Aja API Product", () => {
-  it("Success get product list", async function () {
+  it("Success update product", async function () {
     const response = await request.post("/authentications").send({
       email: "dewi@gmail.com",
       password: "123yayaya",
     });
     const token = response.body.data.accessToken;
-    const getProductListResponse = await request.get("/products").auth(token, { type: "bearer" });
-    expect(getProductListResponse.status).to.eql(200);
+
+    //
+    //
   });
 });
