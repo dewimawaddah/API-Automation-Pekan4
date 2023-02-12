b55cb7ad963012a66b43d30fadc327bfd3cbbb1f
Dewi Mawaddah <dewimawaddahwarrohmah@gmail.com>
2023-02-12 11:03:23 +0700

    done negative deleteProduct dan negative getProductDetail

--- test/deleteProduct.js
+++ test/deleteProduct.js
@@ -12,4 +12,17 @@ describe("Kasir Aja API Product", () => {
     expect(getProductDeleteResponse.body.status).to.eql("success");
     expect(getProductDeleteResponse.body.message).to.eql("Product berhasil dihapus");
   });
+
+  //start negative
+  it("Success delete product dengan id tidak valid", async function () {
+    const response = await request.post("/authentications").send({
+      email: "dewi@gmail.com",
+      password: "123yayaya",
+    });
+    const token = response.body.data.accessToken;
+    const getProductDeleteResponse = await request.delete("/products/5cadf7da-15fa-4848").auth(token, { type: "bearer" });
+    expect(getProductDeleteResponse.body.status).to.eql("fail");
+    expect(getProductDeleteResponse.body.message).to.eql("id tidak valid");
+  });
+  //end negative
 });
--- test/getProductDetail.js
+++ test/getProductDetail.js
@@ -8,9 +8,22 @@ describe("Kasir Aja API Product", () => {
       password: "123yayaya",
     });
     const token = response.body.data.accessToken;
-    const getProductDetailResponse = await request.get("/products/5cadf7da-15fa-4848-98f6-3d86b0ebe643").auth(token, { type: "bearer" });
+    const getProductDetailResponse = await request.get("/products/53c9fecb-c1f7-4bad-b8a5-9de5bfae4485").auth(token, { type: "bearer" });
     expect(getProductDetailResponse.body.status).to.eql("success");
     expect(getProductDetailResponse.body.data.product.code).to.eql("A314ASDDFIER3452");
-    expect(getProductDetailResponse.body.data.product.name).to.eql("Roti");
+    expect(getProductDetailResponse.body.data.product.name).to.eql("Yakult");
   });
+
+  //start negative
+  it("Failed get product detail dengan produk yg sudah dihapus/tidak ditemukan", async function () {
+    const response = await request.post("/authentications").send({
+      email: "dewi@gmail.com",
+      password: "123yayaya",
+    });
+    const token = response.body.data.accessToken;
+    const getProductDetailResponse = await request.get("/products/5cadf7da-15fa-4848-98f6-3d86b0ebe643").auth(token, { type: "bearer" });
+    expect(getProductDetailResponse.body.status).to.eql("fail");
+    expect(getProductDetailResponse.body.message).to.eql("Product tidak ditemukan");
+  });
+  //end negative
 });
