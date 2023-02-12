6bf874577601efdd716b12612fa20f34e5aa55fe
Dewi Mawaddah <dewimawaddahwarrohmah@gmail.com>
2023-02-12 09:19:06 +0700

    done updateProduct

--- test/updateProduct.js
+++ test/updateProduct.js
@@ -10,6 +10,19 @@ describe("Kasir Aja API Product", () => {
     const token = response.body.data.accessToken;
 
     //
+    const updateProductResponse = await request
+      .put("/products/811f547e-a24e-4f94-bfe1-b7ed7d11c026")
+      .send({
+        category_id: "811f547e-a24e-4f94-bfe1-b7ed7d11c026",
+        code: "A314ASDDFIER3452",
+        name: "Roti",
+        price: "12000",
+        cost: "10000",
+        stock: "5",
+      })
+      .auth(token, { type: "bearer" });
+
+    expect(updateProductResponse.body.data.name).to.eql("Roti");
     //
   });
 });
